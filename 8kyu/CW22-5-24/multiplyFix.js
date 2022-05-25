612d3734ce271dedb92199143d1e5c530fd5a745
Sierra Rivas <RivasDev22@Outlook.com>
2022-05-24 23:07:50 -0500

    Add daily challenge for 5-24-22

---
+++ 8kyu/CW22-5-24/multiplyFix.js
@@ -0,0 +1,15 @@
+//RivasDev22 Daily Codewars - Multiply Fix
+
+//This code does not execute properly. Try to figure out why.
+
+//function multiply(a, b){
+//    return ab;
+//}
+
+//Add multiplication opperatior to fix
+
+function multiply(a, b) {
+    return a * b;
+}
+
+  //Passed
