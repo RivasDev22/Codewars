7a1cd013070d394466519f8b99b0b75518d54b36
Sierra Rivas <RivasDev22@Outlook.com>
2022-05-26 17:56:05 -0500

    Add daily challenge for 5-26-22

---
+++ 8kyu/CW22-5-26/sentence.js
@@ -0,0 +1,16 @@
+//RivasDev22 Daily Codewars - Sentence Smash
+
+//Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. 
+
+//You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. 
+
+//Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+//
+//Join array to make sentence
+
+function smash (words) {
+    return words.join(" ");
+ };
+
+ //Passed
