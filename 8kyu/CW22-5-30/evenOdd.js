63d816bc49f29df54d90f4a725c6cb3918dac1a6
Sierra Rivas <RivasDev22@Outlook.com>
2022-05-30 23:38:38 -0500

    Add daily challenge for 5-30-22

---
+++ 8kyu/CW22-5-30/evenOdd.js
@@ -0,0 +1,16 @@
+//RivasDev22 Daily Codewars - Even or Odd
+
+//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+//
+//Find remainder of number after dividing by 2
+
+//If remainder is 0, return Even
+
+//Other amounts return Odd
+
+function even_or_odd(number) {
+    return (number % 2 == 0 ? "Even" : "Odd")
+  }
+
+  //Passed
