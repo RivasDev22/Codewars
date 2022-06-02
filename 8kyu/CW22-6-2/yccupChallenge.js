c9d83baa061e85b62527c3121ea44f5a4604cabc
Sierra Rivas <RivasDev22@Outlook.com>
2022-06-02 08:50:56 -0500

    Add daily challenge for 6-2-22

---
+++ 8kyu/CW22-6-2/yccupChallenge.js
@@ -0,0 +1,12 @@
+//RivasDev22 Daily Codewars - You Can't Code Under Pressure
+
+//Code as fast as you can! You need to double the integer and return it.
+
+//
+//Double i in return
+
+function doubleInteger(i) {
+    return i * 2;
+  }
+
+  //Passed
