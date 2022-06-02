2f88f190bb78b9a6aacbf4dee5e44346de0fc297
Sierra Rivas <RivasDev22@Outlook.com>
2022-06-01 23:59:02 -0500

    Add daily challenge for 6-1-22

---
+++ 8kyu/CW22-6-1/addLength.js
@@ -0,0 +1,12 @@
+//RivasDev22 Daily Codeward - Add Length
+
+//What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array?
+
+//
+//Map new array
+
+function addLength(str){
+    return str.split(' ').map(function(v){return x+' '+x.length})
+  }
+
+  //Passed
