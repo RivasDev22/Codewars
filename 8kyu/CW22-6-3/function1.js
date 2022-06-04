1fc81dcafb3483f4266bbd3ca93cd041f4e36875
Sierra Rivas <RivasDev22@Outlook.com>
2022-06-03 20:22:17 -0500

    Add daily challenge for 6-3-22

---
+++ 8kyu/CW22-6-3/function1.js
@@ -0,0 +1,12 @@
+//RivasDev22 Daily Codewars - Function One - hello world
+
+//Make a simple function called greet that returns the most-famous "hello world!".
+
+//
+//Use return
+
+function greet(){
+    return "hello world!";
+}
+
+//Passed
