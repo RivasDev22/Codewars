f897f039a2d7b1bf9385c72bd34f10d978668d6d
Sierra Rivas <RivasDev22@Outlook.com>
2022-06-06 02:22:14 -0500

    Add daily challenge for 6-6-22

---
+++ 8kyu/CW22-6-6/thirdAngle.js
@@ -0,0 +1,16 @@
+//RivasDev22 Daily Codewars - Third Angle of a Triangle
+
+//You are given two interior angles (in degrees) of a triangle.
+
+//Write a function to return the 3rd.
+
+//Note: only positive integers will be tested.
+
+//
+//Add angles A snd B and subtract from 180
+
+function otherAngle(a, b) {
+    return 180 - (a+b);
+  }
+
+  //Passed
