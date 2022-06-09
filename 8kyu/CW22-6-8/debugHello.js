4739faee0ea1b010639ef123af701269090c836f
Sierra Rivas <RivasDev22@Outlook.com>
2022-06-08 23:01:33 -0500

    Add daily challenge for 6-8-22

---
+++ 8kyu/CW22-6-8/debugHello.js
@@ -0,0 +1,19 @@
+//RivasDev22 Daily Codewars- Grasshopper - Debug sayHello
+
+//The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+//Example output:
+
+//Hello, Mr. Spock
+
+//function sayHello(name) {
+//    return 'Hello'
+//}
+
+//
+//Add spacing between hello and add name to the return
+function sayHello(name) {
+    return 'Hello, ' + name;
+  }
+
+//Passed
