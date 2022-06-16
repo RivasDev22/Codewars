6e6c268f687612fa509b085a9b5aa6385757148f
Sierra Rivas <RivasDev22@Outlook.com>
2022-06-15 23:54:12 -0500

    Add daily challenge for 6-15-22

---
+++ 8kyu/CW22-6-15/simpleMult.js
@@ -0,0 +1,12 @@
+//RivasDev22 Daily Codewars - Simple Multiplication
+
+//This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+//
+//Use if statement to determine number to multiply
+
+function simpleMultiplication(number) {
+    return number % 2 == 0 ? number*8 :number*9;
+}
+
+//Passed
