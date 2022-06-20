1dde71ffe52f1a566bda1f8e0bc4adfc980db8d7
Sierra Rivas <RivasDev22@Outlook.com>
2022-06-19 19:21:30 -0500

    Add daily challenge for 6-19-22

---
+++ 8kyu/CW22-6-19/stringRep2.js
@@ -0,0 +1,12 @@
+//RivasDev22 Daily Codewars - String Repeat Better Solution
+
+//Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+//
+//Use the repeat function to itterate the string n times
+
+function repeatStr (n, s) {
+    return s.repeat(n);
+  }
+
+  //Passed
