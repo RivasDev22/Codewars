8d57a3317d7cbbb4b679585263d8b6490447b322
Sierra Rivas <RivasDev22@Outlook.com>
2022-07-04 17:46:51 -0500

    Add daily challenge for 7-4-22

---
+++ 8kyu/CW22-7-4/myTail.js
@@ -0,0 +1,31 @@
+//RivasDev22 Daily Codewars - Is this my tail?
+
+//Some new animals have arrived at the zoo. The zoo keeper is concerned that perhaps the animals do not have the right tails. To help her, you must correct the broken function to make sure that the second argument (tail), is the same as the last letter of the first argument (body) - otherwise the tail wouldn't fit!
+
+//If the tail is right return true, else return false.
+
+//The arguments will always be strings, and normal letters.
+
+//Given
+//function correctTail(bod, tail ;
+  
+//sub = body.substr(bodylength-(tail.length)
+  
+//if sub = tail) ;
+//  return true
+//}
+//else 
+//  return false
+
+//Got rid of substring to check just using endsWith()
+
+function correctTail(body, tail){
+    if (body.endsWith(tail)){
+      return true
+    }
+    else {
+      return false
+    }
+  }
+
+//Passed
