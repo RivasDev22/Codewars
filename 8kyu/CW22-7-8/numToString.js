2e83c1bff6e5338e4f42eade21f6126be8f75c27
Sierra Rivas <RivasDev22@Outlook.com>
2022-07-08 23:54:44 -0500

    Add daily challenge for 7-8-22

---
+++ 8kyu/CW22-7-8/numToString.js
@@ -0,0 +1,14 @@
+//RivasDev22 Daily Codewars - Convert a Number to a String!
+
+//We need a function that can transform a number into a string.
+
+//What ways of achieving this do you know?
+
+//
+//Use toString() to convert number
+
+function numberToString(num) {
+    return num.toString()
+  }
+
+//Passed
