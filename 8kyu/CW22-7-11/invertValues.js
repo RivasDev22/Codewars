f095ee2ec717da8dce7208d15e033de01742bce4
Sierra Rivas <RivasDev22@Outlook.com>
2022-07-11 22:14:10 -0500

    Add daily challenge for 7-11-22

---
+++ 8kyu/CW22-7-11/invertValues.js
@@ -0,0 +1,12 @@
+//RivasDev22 Daily Codewars - Invert Values
+
+//Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+//
+//Return the map of array with the elements multiplied by -1
+
+function invert(array) {
+    return array.map(x => x * -1);
+ }
+
+//Passed
