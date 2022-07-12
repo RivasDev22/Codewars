d6237094b99c10ef3354413b4c9b6e84eb04b9e5
Sierra Rivas <RivasDev22@Outlook.com>
2022-07-12 12:34:32 -0500

    Add daily challenge for 7-12-22

---
+++ 8kyu/CW22-7-12/doubleArray.js
@@ -0,0 +1,12 @@
+//RivasDev22 Daily Codewars - Lost Without a Map
+
+//Given an array of integers, return a new array with each value doubled.
+
+//
+//Return the map of array with the elements multiplied by 2
+
+function maps(x){
+    return x.map(e => e * 2);
+  }
+
+//Passed
