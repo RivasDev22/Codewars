6b3fdb1b9c1849070139f3b5c8eaf7bac48cc84e
Sierra Rivas <RivasDev22@Outlook.com>
2022-07-15 15:17:57 -0500

    Add daily challenge for 7-15-22

---
+++ 8kyu/CW22-7-15/boolToString.js
@@ -0,0 +1,12 @@
+//RivasDev22 Daily Codewars - Convert a Boolean to a String
+
+//Implement a function which convert the given boolean value into its string representation.
+
+//
+//If b is true, return the string 'true' and same for false
+
+function booleanToString(b){
+    return b ? "true" : "false"; 
+  }
+
+//Passed
