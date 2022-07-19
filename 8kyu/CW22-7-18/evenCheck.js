22d928978d2e3009a180e7f82abc77f660ed4c3a
Sierra Rivas <RivasDev22@Outlook.com>
2022-07-18 23:39:55 -0500

    Add daily challenge for 7-18-22

---
+++ 8kyu/CW22-7-18/evenCheck.js
@@ -0,0 +1,18 @@
+//RivasDev22 Daily Codewars -Is It Even?
+
+//In this Kata we are passing a number (n) into a function.
+
+//Your code will determine if the number passed is even (or not).
+
+//The function needs to return either a true or false.
+
+//Numbers may be positive or negative, integers or floats.
+
+//
+//If remainder of 2 is 0, return true
+
+function testEven(n) {
+    return (n % 2 == 0 ? true : false);
+}
+
+//Passed
