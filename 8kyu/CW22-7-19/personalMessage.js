d06100d60aa9f0e722e0d88cc087461076aa7ae5
Sierra Rivas <RivasDev22@Outlook.com>
2022-07-19 00:14:35 -0500

    Add daily challenge for 7-19-22

---
+++ 8kyu/CW22-7-19/personalMessage.js
@@ -0,0 +1,12 @@
+//RivasDev22 Daily Codewars - Grasshopper Personalized Message
+
+//Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+//
+//If the name is equal to owner name return boss message, otherwise return Hello guest
+
+function greet (name, owner) {
+    return name == owner ? "Hello boss" : "Hello guest";
+  }
+
+//Passed
