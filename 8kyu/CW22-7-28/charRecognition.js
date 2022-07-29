35fb168559260c62457d2ee5adbd9e6f02ea43e2
Sierra Rivas <RivasDev22@Outlook.com>
2022-07-28 22:14:19 -0500

    Add daily challenge for 7-22-28

---
+++ 8kyu/CW22-7-28/charRecognition.js
@@ -0,0 +1,30 @@
+//RivasDev22 Daily Codewars - Correct the mistakes of the character recognition software
+
+//Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+//When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+//Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+//S is misinterpreted as 5, O is misinterpreted as 0, I is misinterpreted as 1
+
+//The test cases contain numbers only by mistake.
+
+//
+//Pr: string - string with numbers as characters
+
+//Rt: corrected - new string with the numbers replaced
+
+//Ex: correct("L0ND0N") returns "LONDON"
+
+function correct(string)
+{
+  let corrected = string
+	.replace(/5/g, "S")
+  .replace(/0/g, "O")
+  .replace(/1/g, "I");
+  
+  return corrected;
+}
+
+//Passed
