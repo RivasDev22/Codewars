23fb75b59d68a444fca84b3b7007c8b6404811c5
Sierra Rivas <RivasDev22@Outlook.com>
2022-07-30 22:27:15 -0500

    Add daily challenge for 7-22-30

---
+++ 8kyu/CW22-7-30/makeUpperCase.js
@@ -0,0 +1,17 @@
+//RivasDev22 Daily Codewars - MakeUpperCase
+
+//Write a function which converts the input string to uppercase.
+
+//
+//Pr: str - string to capitalize
+
+//Rt: capitalized string
+
+//Ex: makeUpperCase("hello") returns "HELLO"
+
+function makeUpperCase(str) {
+    //Use toUpperCase function
+    return str.toUpperCase();
+  }
+
+//Passed
