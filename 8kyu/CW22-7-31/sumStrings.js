1dab1fd8303d83513066f68a72ff6184cbdaf1a5
Sierra Rivas <RivasDev22@Outlook.com>
2022-07-31 23:17:48 -0500

    Add daily challenge for 7-22-31

---
+++ 8kyu/CW22-7-31/sumStrings.js
@@ -0,0 +1,17 @@
+//RivasDev22 Daily Codewars - Sum The Strings
+
+//Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string)
+
+//
+//Pr: a, b - two strings containing number
+
+//Rt: string of the total
+
+//Ex: sumStr("34","5") returns "39"
+
+function sumStr(a,b) {
+    //Convert a and b number total to a string
+    return String(Number(a)+Number(b));
+  }
+
+  //Passed
