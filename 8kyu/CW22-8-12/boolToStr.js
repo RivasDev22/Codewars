02f2c9bc2dad8afc9f66d82a50fac4ce7a0be7fc
Sierra Rivas <RivasDev22@Outlook.com>
2022-08-12 20:18:34 -0500

    Add daily challenge for 8-12-22

---
+++ 8kyu/CW22-8-12/boolToStr.js
@@ -0,0 +1,19 @@
+//RivasDev22 Daily Codewars - Convert boolean values to strings 'Yes' or 'No'
+
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+//
+//Pr: bool - input boolean tostring
+
+//Rt: 'Yes' if true, 'No' if false
+
+//Ex: boolToWord(true), 'Yes'
+
+//
+
+function boolToWord( bool ){
+    //If bool is true, return 'Yes', else return 'No'
+    return bool == 1 ? "Yes" : "No"
+  }
+
+//Passed
