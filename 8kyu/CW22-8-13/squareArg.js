58348557ecab66496be23ab1a25d192ca3ee2da2
Sierra Rivas <RivasDev22@Outlook.com>
2022-08-13 15:37:02 -0500

    Add daily challenge for 8-13-22

---
+++ 8kyu/CW22-8-13/squareArg.js
@@ -0,0 +1,19 @@
+//RivasDev22 Daily Codewars - Function 2 - squaring an argument
+
+//Now you have to write a function that takes an argument and returns the square of it.
+
+//
+//Pr: num - input to square
+
+//Rt: squared value of num
+
+//Ex: square(3) returns 9
+
+//
+
+function square(num){
+    //Return value multiplied by itself
+    return num * num
+  }
+
+//Passed
