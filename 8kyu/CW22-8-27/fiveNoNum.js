c556c031abdde29767b0491d3094b849479b5bd9
Sierra Rivas <RivasDev22@Outlook.com>
2022-08-27 09:17:41 -0500

    Add daily challenge for 8-27-22

---
+++ 8kyu/CW22-8-27/fiveNoNum.js
@@ -0,0 +1,20 @@
+//RivasDev22 Daily Codewars - 5 Without Numbers!!
+
+//Write a function that always returns 5
+
+//Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+//
+
+//Rt: 5 without using forbidden characters
+
+//Ex: returns 5
+
+//
+
+function unusualFive() {
+    //Get the length of a str with 5 characters
+    return 'solve'.length;
+  }
+
+//Passed
