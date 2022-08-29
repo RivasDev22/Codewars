f0ba8a9864bc85cfdc0b267b3a2e0b0bc219a91d
Sierra Rivas <RivasDev22@Outlook.com>
2022-08-28 19:03:25 -0500

    Add daily challenge for 8-28-22

---
+++ 8kyu/CW22-8-28/messiGoals.js
@@ -0,0 +1,25 @@
+//RivasDev22 Daily Codewars - Grasshopper - Messi Goals Function
+
+//Messi is a soccer player with goals in three leagues:
+
+//LaLiga, Copa del Rey, Champions
+
+//Complete the function to return his total number of goals in all three leagues.
+
+//Note: the input will always be valid.
+
+//
+//Pr: laLigaGoals, copaDelRey, championsLeagueGoals - ints of number of goals scored
+
+//Rt: total goals earned
+
+//Ex: goals(43, 10, 5) returns 58
+
+//
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    //add all values for total goals
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  }
+
+//Passed
