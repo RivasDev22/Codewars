cdd61d20ccd4cd692038c1852eb46868cd87b945
Sierra Rivas <RivasDev22@Outlook.com>
2023-02-07 23:59:26 -0600

    Add daily challenge for 02-07-23

---
+++ 8kyu/CW23-02-07/oppositeNumber.js
@@ -0,0 +1,13 @@
+//RivasDev22 Daily Codewars - Opposite Number
+
+//Very simple, given an integer or a floating-point number, find its opposite.
+
+//
+//Pr: number input
+
+//Ex: opposite(4.25) returns -4.25
+
+function opposite(number) {
+    return number * -1;
+    //multiply input by -1
+  }
