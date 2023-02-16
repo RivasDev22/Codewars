0ddeed54a6a8df1a57bf8293ada4b3d608834992
Sierra Rivas <RivasDev22@Outlook.com>
2023-02-15 23:40:13 -0600

    Add daily challenge for 02-15-23

---
+++ 8kyu/CW23-02-15/removeStrSpaces.js
@@ -0,0 +1,10 @@
+//RivasDev22 Daily Codewars - Covfefe
+
+//Simple, remove the spaces from the string, then return the resultant string.
+
+//
+//
+
+function noSpace(x){
+    return x.replace(/ /g, "")
+  }
